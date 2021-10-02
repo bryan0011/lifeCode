3be5bf6a53f70a66e7e821bbc16bbb9bc92eaeb0
Bryan Kuo <bryan0011@gmail.com>
2021-10-02 16:11:33 +0800

    Initial Commit

---
+++ lifeCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifeCode
+//
+//  Created by Bryan Kuo on 2021/10/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
